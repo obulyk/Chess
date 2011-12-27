b10959ebbdb82a561360313e2411164bd90f2cc2
unknown <Dennie@.(none)>
2011-12-27 23:15:32 +0200

    Dennis:

--- Totalbeginner/src/org/totalbeginner/tutorial/FirstClass.java
+++ Totalbeginner/src/org/totalbeginner/tutorial/FirstClass.java
@@ -3,7 +3,7 @@ package org.totalbeginner.tutorial;
 import javax.swing.JFrame;
 
 //import java.io;
-
+ 
 public class FirstClass {
 	public static void main(String[] args)
 	{
@@ -14,6 +14,5 @@ public class FirstClass {
 		//frame.println("fefse");
 	System.out.println ("hello world, something added:)") ;
 	System.out.println ("and one more change, just in case...)") ;
-
 	}	
 }
