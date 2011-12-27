6037342c65a0cdd9d81b32cebcf953bb79c47eae
O Bulyk <bulyk.oksana@gmail.com>
2011-12-27 21:02:07 +0200

    First Commit

---
+++ Totalbeginner/src/org/totalbeginner/tutorial/FirstClass.java
@@ -0,0 +1,17 @@
+package org.totalbeginner.tutorial;
+
+import javax.swing.JFrame;
+
+//import java.io;
+
+public class FirstClass {
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("Window");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		//frame.println("fefse");
+	System.out.println ("hello world") ;
+	}	
+}
